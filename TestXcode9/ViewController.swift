6b56e9bfd59d781e0d73554271f1510e5a5d7588
Ajisaputrars <ajisaputrars@gmail.com>
2017-11-14 09:56:06 +0700

    Initial Commit

--- TestXcode9/ViewController.swift
+++ TestXcode9/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var darkBlueBG: UIImageView!
+    @IBOutlet weak var powerBtn: UIButton!
+    @IBOutlet weak var cloudHolder: UIView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var hustleLbl: UILabel!
+    @IBOutlet weak var onLbl: UILabel!
+    
+    var player: AVAudioPlayer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        preparingPlayerToPlay()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func powerButtonPressed(_ sender: UIButton) {
+        cloudHolder.isHidden = false
+        darkBlueBG.isHidden = false
+        powerBtn.isHidden = true
+        
+        player.play()
+        
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 20, width: 233, height: 209)
+        }) { (finished) in
+            self.onLbl.isHidden = false
+            self.hustleLbl.isHidden = false
+        }
     }
+    
+    func preparingPlayerToPlay(){
+        let path = Bundle.main.path(forResource: "hustle-ons", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
 
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let err as NSError{
+            print("Errornya di sini nich\(err.description)")
+        }
 
+    }
 }
-
